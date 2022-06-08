e63d2c39d0606b3af7a7f17b5e07643991496bc5
dfg1425@naver.com <dfg1425@naver.com>
2022-06-08 22:04:09 +0900

    App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import ConsumerEx from "./components/Consumer";
+import Static from "./components/StaticEx";
+import FunctionalEx from "./components/Functional";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ConsumerEx />
+        <Static />
+        <FunctionalEx />
       </header>
     </div>
   );
